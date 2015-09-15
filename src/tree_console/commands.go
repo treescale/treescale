a3556cdbde5c34082318de3eeef4782778338d51
Tigran Bayburtsyan <tigran@bayburtsyan.com>
2015-09-15 18:50:52 +0400

    command line testings with flags

--- src/tree_console/commands.go
+++ src/tree_console/commands.go
@@ -6,6 +6,7 @@ import (
 
 var (
 	TreeScaleCMD	*cobra.Command
+	KX	*[]string
 )
 
 func init() {
@@ -14,6 +15,9 @@ func init() {
 		Short:	"Scaling and Resource Managaement evented system, based on Mathematical Tree and Graph",
 		Long:	"",
 	}
+
+	// Adding Flags
+
 
 	// List of commands to execute
 	version := &cobra.Command{
@@ -26,6 +30,20 @@ func init() {
 		},
 	}
 
+	// Configuration commands
+	config := &cobra.Command{
+		Use: 	"config [commands]",
+		Short:	"Command for handling configuration tools",
+	}
+	compile_config := &cobra.Command{
+		Use: "compile [options]",
+		Short: "Compiles multiple config files",
+		Long: "Compiles multiple config files into one single TreeScale database for sending to nodes",
+		Run: CompileConfig,
+	}
+	compile_config.Flags().StringP("path", "p", ".", "Test Usage")
+	compile_config.Flags().StringSliceP("pp", "l", []string{"."}, "Test Usage")
+	config.AddCommand(compile_config)
 
-	TreeScaleCMD.AddCommand(version)
+	TreeScaleCMD.AddCommand(version, config)
 }
--- src/tree_console/config.go
+++ src/tree_console/config.go
@@ -1,2 +1,18 @@
 package tree_console
+import (
+	"tree_node/node_info"
+	"github.com/spf13/cobra"
+	"fmt"
+)
 
+
+type TreeScaleConf struct {
+	SSH				map[string]SSHConfig				`toml:"ssh" json:"ssh" yaml:"ssh"`
+	TreeNode		map[string]node_info.NodeInfo		`toml:"tree_node" json:"tree_node" yaml:"tree_node"`
+	// TODO: Add docker registry config here
+	// TODO: Add balancer config here
+}
+
+func CompileConfig(cmd *cobra.Command, args []string) {
+	fmt.Println(cmd.Flag("pp").Value)
+}
