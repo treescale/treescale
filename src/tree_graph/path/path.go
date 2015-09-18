87e2fd71a374969723c07e5bd51ea7e8856b7661
David Harutyunyan <davidhn96@gmail.com>
2015-09-18 19:33:04 +0400

    message length bug fixed

--- src/tree_graph/path/path.go
+++ src/tree_graph/path/path.go
@@ -16,7 +16,7 @@ type Path struct {
 func PathFromMessage(msg []byte) (body_index int, p Path, err error) {
 	// First 4 bytes in message is a length of json encoded Path
 	path_len := int(binary.LittleEndian.Uint32(msg[:4]))
-	err = ffjson.Unmarshal(msg[4:path_len], &p)
+	err = ffjson.Unmarshal(msg[4:path_len+4], &p)
 	body_index = 4 + path_len
 	return
 }
