6abe479d5fe6c0f3f5ea984284ba3200b3b69e87
Tigran Bayburtsyan <tigran@bayburtsyan.com>
2016-10-21 12:26:21 +0400

    starting from scratch

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,3 @@
-extern crate mio;
-
-mod error;
-mod network;
-mod node;
-
-use node::Node;
-use std::thread;
-use std::time;
-use std::env;
-
 fn main() {
-    if env::args().nth(1) == Some(String::from("n1")) {
-        let mut n = Node::new(false, 1, String::from("2").as_bytes());
-        n.run("0.0.0.0:8888");
-        n.connect("127.0.0.1:8889");
-        thread::sleep(time::Duration::from_secs(5));
-        n.write_str(String::from("Test Data").as_bytes(), String::from("5"));
-        thread::sleep(time::Duration::from_secs(25));
-    } else {
-        let mut n = Node::new(false, 1, String::from("2").as_bytes());
-        n.run("0.0.0.0:8889");
-        // n.connect("127.0.0.1:8888");
-        // thread::sleep(time::Duration::from_secs(5));
-        // n.write_str(String::from("Test Data").as_bytes(), String::from("5"));
-        thread::sleep(time::Duration::from_secs(25));
-    }
+
 }
