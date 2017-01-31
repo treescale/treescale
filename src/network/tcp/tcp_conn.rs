9b20851dd239927f2cfa90bc7b5900566bf8fc65
Tigran Bayburtsyan <tigran@bayburtsyan.com>
2017-01-31 11:33:11 +0400

    tcp connection read prime value and general read data

--- src/network/tcp/tcp_conn.rs
+++ src/network/tcp/tcp_conn.rs
@@ -74,18 +74,25 @@ impl TcpReaderConn {
         };
 
         self.endian_bytes_index += read_len;
+        if self.endian_bytes_index > 4 {
+            warn!("Pending data index bigger than pending data length, closing connection -> Pending Index: {} , Data Lenght {}"
+            , self.endian_bytes_index, 4);
+            return (true, None);
+        }
         // if we still have some data to read
-        if self.endian_bytes_index < self.endian_bytes_index {
+        if self.endian_bytes_index < 4 {
             return (false, Some(0));
         }
 
         // if we got here then just setting BigEndian bytes and returning parsed number
+        self.endian_bytes_index = 0;
         (true, Some(unsafe {
             let a = [self.endian_bytes[0]
                       , self.endian_bytes[1]
                       , self.endian_bytes[2]
                       , self.endian_bytes[3]];
             let endian_num = mem::transmute::<[u8; 4], u32>(a);
+            self.endian_bytes = vec![0; 4];
             u32::from_be(endian_num)
         }))
     }
@@ -119,5 +126,102 @@ impl TcpReaderConn {
         self.api_version = version.unwrap();
 
         Some(true)
+    }
+
+    /// Reading Unique Prime Value from TCP Socket
+    /// Function will return 'None' if there is a
+    /// problem with connection and it need to be closed
+    #[inline(always)]
+    pub fn read_prime_value(&mut self) -> Option<bool> {
+        // trying to read Prime Value as 8 bytes
+        if self.pending_length == 0 {
+            self.pending_length = 8;
+            self.pending_data = vec![vec![0; 8]];
+        }
+
+        let read_len = match self.socket.read(&mut self.pending_data[0][self.pending_index..]) {
+            Ok(n) => n,
+            Err(e) => {
+                // if we got WouldBlock, then this is Non Blocking socket
+                // and data still not available for this, so it's not a connection error
+                if e.kind() == ErrorKind::WouldBlock {
+                    return Some(false);
+                }
+
+                return None;
+            }
+        };
+
+        self.pending_index += read_len;
+        if self.pending_index > self.pending_length {
+            warn!("Pending data index bigger than pending data length, closing connection -> Pending Index: {} , Data Lenght {}"
+            , self.pending_index, self.pending_length);
+            return None;
+        }
+
+        if self.pending_index < self.pending_length {
+            return Some(false);
+        }
+
+        // if we got here then we have all data
+        self.value = unsafe {
+            let data = self.pending_data.remove(0);
+            let a: [u8; 8] = [data[0], data[1], data[2], data[3], data[4], data[5], data[6], data[7]];
+            u64::from_be(mem::transmute::<[u8; 8], u64>(a))
+        };
+
+        self.pending_length = 0;
+        self.pending_index = 0;
+        Some(true)
+    }
+
+    /// Reading next chunck of data from TCP socket
+    /// It is based on our [BigEndian Length]:[Raw Data] API combination
+    #[inline(always)]
+    pub fn read_data(&mut self) -> (bool, Option<Vec<u8>>) {
+        if self.pending_length == 0 {
+            let (done, length) = self.read_big_endian();
+            if !done {
+                return (false, None);
+            }
+
+            // if there is a problem reading big endian number
+            // notifying to close connection
+            if length.is_none() {
+                return (true, None);
+            }
+
+            // if we got here then we have pending data length
+            self.pending_length = length.unwrap() as usize;
+            self.pending_data = vec![vec![0; self.pending_length]];
+        }
+
+        let read_len = match self.socket.read(&mut self.pending_data[0][self.pending_index..]) {
+            Ok(n) => n,
+            Err(e) => {
+                // if we got WouldBlock, then this is Non Blocking socket
+                // and data still not available for this, so it's not a connection error
+                if e.kind() == ErrorKind::WouldBlock {
+                    return (false, None);
+                }
+
+                return (true, None);
+            }
+        };
+
+        self.pending_index += read_len;
+        if self.pending_index > self.pending_length {
+            warn!("Pending data index bigger than pending data length, closing connection -> Pending Index: {} , Data Lenght {}"
+            , self.pending_index, self.pending_length);
+            return (true, None);
+        }
+
+        if self.pending_index < self.pending_length {
+            return (false, None);
+        }
+
+        self.pending_length = 0;
+        self.pending_index = 0;
+        (true, Some(self.pending_data.remove(0)))
     }
 }
