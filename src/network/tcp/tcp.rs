a112e442797d70fa85acc323d92bfe1a6b6fd528
Tigran Bayburtsyan <tigran@bayburtsyan.com>
2017-03-03 23:41:51 +0400

    making basic networking structure

--- src/network/tcp/tcp.rs
+++ src/network/tcp/tcp.rs
@@ -3,10 +3,11 @@ extern crate slab;
 extern crate mio;
 
 use self::mio::Token;
+use self::mio::tcp::TcpListener;
 
 type Slab<T> = slab::Slab<T, Token>;
 
 // Main struct to handle TCP networking
 pub struct TcpNetwork {
-    
+    server_socket: TcpListener,
 }
