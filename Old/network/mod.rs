f4afb58c32eb6f1cc47e026cb825937b8468286a
Tigran Bayburtsyan <tigran@bayburtsyan.com>
2017-03-14 14:01:43 +0400

    cleaning up old code

--- Old/network/mod.rs
+++
@@ -1,6 +0,0 @@
-mod net;
-mod conn;
-mod tcp;
-
-pub use self::conn::{Connection, ConnectionType};
-pub use self::net::{Network, NetworkCommand, NetworkCMD};
--- Old/network/tcp/mod.rs
+++
@@ -1,9 +0,0 @@
-mod net;
-mod reader;
-mod writer;
-mod tcp_conn;
-
-pub use self::reader::{TcpReader, TcpReaderCommand, TcpReaderCMD};
-pub use self::writer::{TcpWriter, TcpWriterCommand, TcpWriterCMD};
-pub use self::net::{TcpNetwork, TcpNetworkCommand, TcpNetworkCMD};
-pub use self::tcp_conn::{TcpReaderConn, TcpWriterConn};
