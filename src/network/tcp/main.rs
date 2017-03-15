d46a9d8d55f8e35b29a31c7e64e0c92814071ce3
Tigran Bayburtsyan <tigran@bayburtsyan.com>
2017-03-15 18:34:28 +0400

    tcp server implementation

--- src/network/tcp/main.rs
+++ src/network/tcp/main.rs
@@ -1,12 +1,54 @@
 #![allow(dead_code)]
+extern crate mio;
 
-use node::Node;
+use helper::Log;
 
+use self::mio::tcp::TcpListener;
+use self::mio::{Ready, PollOpt};
+use node::{Node, NET_TCP_SERVER_TOKEN};
+
+use std::net::SocketAddr;
+use std::error::Error;
+use std::process;
+use std::str::FromStr;
+
+/// TcpNetwork Trait for implementing TCP networking capabilities
+/// On top of Node structure
 pub trait TcpNetwork {
+    /// Min function to attach TCP service functionality to existing POLL service
+    fn register_tcp(&mut self);
 
+    /// Make TCP server socket listener from given address
+    fn make_tcp_server(address: &str) -> TcpListener;
 }
 
 impl TcpNetwork for Node {
+    fn register_tcp(&mut self) {
+        match self.poll.register(&self.net_tcp_server, NET_TCP_SERVER_TOKEN, Ready::readable(), PollOpt::edge()) {
+            Ok(_) => {}
+            Err(e) => {
+                Log::error("Unable to register TCP server to Node POLL service", e.description());
+                process::exit(1);
+            }
+        }
+    }
 
+    fn make_tcp_server(address: &str) -> TcpListener {
+        let addr = match SocketAddr::from_str(address) {
+            Ok(a) => a,
+            Err(e) => {
+                Log::error("Unable to parse given TCP server address", e.description());
+                process::exit(1);
+            }
+        };
+
+        match TcpListener::bind(&addr) {
+            Ok(s) => s,
+            Err(e) => {
+                Log::error("Unable to bind given TCP server address", e.description());
+                process::exit(1);
+            }
+        }
+    }
 }
 
--- src/node/mod.rs
+++ src/node/mod.rs
@@ -10,6 +10,7 @@ use self::mio::Token;
 use std::u32::MAX as u32MAX;
 
 pub const NET_RECEIVER_CHANNEL_TOKEN: Token = Token((u32MAX - 1) as usize);
-pub const EVENT_RECEIVER_CHANNEL_TOKEN: Token = Token((u32MAX - 2) as usize);
+pub const NET_TCP_SERVER_TOKEN: Token = Token((u32MAX - 2) as usize);
+pub const EVENT_RECEIVER_CHANNEL_TOKEN: Token = Token((u32MAX - 3) as usize);
 
 pub const EVENT_LOOP_EVENTS_SIZE: usize = 15000;
